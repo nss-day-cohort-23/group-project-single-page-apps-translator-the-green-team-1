d5c9e3192ec19b772f82e167c8be18fcbf74a448
Tim A. Conner <timaconner1@gmail.com>
2017-12-06 16:29:21 -0600

    Completed activateButton and populateHTML.

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,20 +1,27 @@
 "use strict";
 
 const translate = require('./translate');
-//const view = require('./view');
+const view = require('./view');
 
 const activateButton = () => {
     let button = document.getElementById("translate-button");
     button.addEventListener("click", function(){
         let selectDropdown = document.getElementById("language-select");
-       // let selectedLanguage = selectDropdown.options[selectDropdown.selectedIndex].value;
-        
+        let selectedLanguage = selectDropdown.options[selectDropdown.selectedIndex].value;
+        let textToTranslate = document.getElementById("translation-text").value;
+        view.displayTranslation(translate.translate(textToTranslate, selectedLanguage));
     });
 };
 
 const populateHTML = () => {
+    let selectDropdown = document.getElementById("language-select");
     let languages = translate.getLanguages();
-    //
+    languages.forEach(element => {
+        let optionsDom = document.createElement('option');
+        optionsDom.value = element;
+        optionsDom.text = element;
+        selectDropdown.appendChild(optionsDom);
+    });
 };
 
 module.exports.createPage = () => {
