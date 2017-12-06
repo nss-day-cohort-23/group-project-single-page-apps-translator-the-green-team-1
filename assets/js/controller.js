9b34b4b118b30161d812297ae323d40112094b98
Tim A. Conner <timaconner1@gmail.com>
2017-12-06 15:49:01 -0600

    Added controller.js functions structure.

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,16 +1,17 @@
 "use strict";
 
-const translate = require('translate');
-const view = require('view');
+const translate = require('./translate');
+const view = require('./view');
+
+const populateHTML = () => {
+    console.log("hello");
+};
+
+const activateButton = () => {
+    console.log("hello");
+};
 
 module.exports.createPage = () => {
-
-};
-
-populateHTML = () => {
-
-};
-
-activeButton = () => {
-
+    populateHTML();
+    activateButton();
 };
