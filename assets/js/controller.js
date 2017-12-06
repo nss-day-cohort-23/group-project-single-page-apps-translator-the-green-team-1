c47fd4c7e6eb297f2d60d52847f9068202198c76
Tim A. Conner <timaconner1@gmail.com>
2017-12-06 16:08:35 -0600

    Add populateHTML()

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -1,15 +1,20 @@
 "use strict";
 
 const translate = require('./translate');
-const view = require('./view');
-
-const populateHTML = () => {
-    //view.displayDropdownItmes();
-};
+//const view = require('./view');
 
 const activateButton = () => {
     let button = document.getElementById("translate-button");
-    //button.addEventListener("")
+    button.addEventListener("click", function(){
+        let selectDropdown = document.getElementById("language-select");
+       // let selectedLanguage = selectDropdown.options[selectDropdown.selectedIndex].value;
+        
+    });
+};
+
+const populateHTML = () => {
+    let languages = translate.getLanguages();
+    //
 };
 
 module.exports.createPage = () => {
