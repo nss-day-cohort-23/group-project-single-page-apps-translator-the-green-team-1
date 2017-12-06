3c7b9305822da17705264889896795aebfbfefbc
Tim A. Conner <timaconner1@gmail.com>
2017-12-06 15:06:24 -0600

    Add example language module with lexicon.

---
+++ assets/js/spanish.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let lexicon = {
+    "merry": "feliz",
+    "politically": "políticamente",
+    "correct": "correcto",
+    "holiday": "fiesta",
+    "my": "mi",
+    "dude": "amigx"
+};
+
+module.exports.getLexicon = () => {
+    return lexicon;
+};
