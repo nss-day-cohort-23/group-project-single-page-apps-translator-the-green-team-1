77053232e90ce384849776682b1aecd5c163a8df
Tim A. Conner <timaconner1@gmail.com>
2017-12-07 11:29:15 -0600

    Refactored translate.js

--- assets/js/translate.js
+++ assets/js/translate.js
@@ -1,8 +1,9 @@
 "use strict";
 
-function translate(text, destLang) {
+// Translates text into destLang.  If word is found, translate it, if the word is not found, keep the untranslated word in the translated text.
+module.exports.translate = (text, destLang) => {
     let lang;
-    // assumption: destination languages = ["spanish", "zulu", "gaelic", "welsh"]
+
     switch(destLang) {
         case "zulu":
             lang = require("./zulu");
@@ -16,20 +17,30 @@ function translate(text, destLang) {
         case "gaelic":
             lang = require("./gaelic");
     }
-    let lexicon = lang.getLexicon();
-    let sentence = text.split(" ");
-    let translation = sentence.map(word => lexicon[word.toLowerCase().replace(/['\",!.]/, "")] !== undefined ? lexicon[word.toLowerCase().replace(/['\",!.]/, "")] : word).join(" ");
+
+    let lexicon = lang.getLexicon(),
+        sentenceArray = text.split(" "),
+        translation = sentenceArray.map(word => canTranslate(lexicon, word) ? lexicon[cleanWord(word)] : word ).join(" ");
 
     return translation;
-}
+};
 
-function getLanguages() {
+// Checks if a word is in the lexicon.  Returns true if word is in lexicon, false if it is not.
+const canTranslate = (lexicon, word) => {
+    return lexicon[cleanWord(word)] !== undefined;
+};
+
+// Returns string to  lower case and removes extra signs that can't be translated
+const cleanWord = (word) => {
+    return word.toLowerCase().replace(/['\",!.]/, "");
+};
+
+// Returns a list of possible languages to translate from
+module.exports.getLanguages = () => {
     return [
         "zulu",
         "spanish",
         "gaelic",
         "welsh"
     ];
-}
-
-module.exports = {translate, getLanguages};
+};
