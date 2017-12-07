cc1007649e6bd044aa6a873b93499ac66e421ce6
Kenzie Bottoms <kenziebottoms@gmail.com>
2017-12-07 11:30:45 -0600

    commented functions, sep translation into variable

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -3,6 +3,7 @@
 const translator = require('./translate');
 const viewer = require('./view');
 
+// set button and text input event listeners
 const activateEvents = () => {
     let button = document.getElementById("translate-button");
     button.addEventListener("click", processUserInput);
@@ -10,12 +11,16 @@ const activateEvents = () => {
     textInput.addEventListener("keyup", processUserInput);
 };
 
+// retrieve and translate input value
 const processUserInput = () => {
     let selectedLanguage = getRadioValue();
     let textToTranslate = document.getElementById("translation-text").value;
-    viewer.displayTranslation(translator.translate(textToTranslate, selectedLanguage), selectedLanguage);
+    let translation = translator.translate(textToTranslate, selectedLanguage);
+    viewer.displayTranslation(translation);
+    viewer.speakTranslation(translation, selectedLanguage);
 };
 
+// return the id of the selected radio button
 const getRadioValue = () => {
     let radios = document.getElementsByClassName("radio");
     let lang = false;
@@ -28,10 +33,12 @@ const getRadioValue = () => {
     return lang;
 };
 
+// populate language choices
 const populateHTML = () => {
     viewer.displayDropdownItems(translator.getLanguages());
 };
 
+// create elements and activate event listeners on them
 module.exports.createPage = () => {
     populateHTML();
     activateEvents();
